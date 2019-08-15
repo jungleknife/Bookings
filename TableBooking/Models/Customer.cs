473bcc02a554b7a0960f2c282b471894713262ba
jungleknife <tony.marchetto@hmrc.gsi.gov.uk>
2019-08-15 14:59:08 +0100

    Added validation to Customer model

--- TableBooking/Models/Customer.cs
+++ TableBooking/Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +11,19 @@ namespace TableBooking.Data
     {
         public int CustomerID { get; set; }
         public ICollection<Booking> Bookings { get; set; }
+        [DisplayName("First name")]
+        [Display(Prompt ="Enter first name")]
+        [MinLength(2), MaxLength(30)]
         public string FirstName { get; set; }
+        [DisplayName("Last name")]
+        [Required(ErrorMessage = "Please fill in your last name")]
         public string LastName { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Mobile number is required")]
+        [RegularExpression(@"^(07\d{9}|447\d{9})$",
+            ErrorMessage = "Please enter a valid mobile number")]
         public string Mobile { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
